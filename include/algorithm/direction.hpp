cada1de27f1bfb85f12b198d69a6c1f3660854bb
Yudi Matsuzake <yudi.matsuzake@gmail.com>
2017-09-14 12:22:56 -0300

    Implement the function classify
    * Implement the function classify;
    
    * Add the vertex_type type;
    
    * Change the functions is_ear, is_above, is_below, is_mouth and
    is_reflex to the direction.hpp file.
    
    change the file of the functions is_ear, is_ear

--- include/algorithm/direction.hpp
+++ include/algorithm/direction.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "polygon.hpp"
 #include "vec.hpp"
 #include "misc.hpp"
 #include "pi.hpp"
@@ -11,6 +12,14 @@ typedef enum direction {
 	RIGHT,
 	ON
 } direction;
+
+typedef enum vertex_type {
+	REGULAR,
+	START,
+	END,
+	SPLIT,
+	MERGE
+} vertex_type;
 
 /** Calculates the direction of point `p2` in reference
   * of segment of `p0` and `p1`
@@ -65,6 +74,172 @@ double angle_to_left(
 		return 2*pi - minor_angle;
 
 	return minor_angle;
+}
+
+/**
+  * check if an vetex `i` of the polygon `poly` is
+  * reflex
+  * TODO: this function consider the polygon was created
+  * in anti-clockwise; need to check that
+  */
+template<typename T, std::size_t n_dimension>
+bool is_reflex(const polygon<T, n_dimension>& poly, std::size_t i)
+{
+	if(poly.size() < 3)
+		return false;
+
+	size_t next_index = (i + 1) % poly.size();
+	size_t prev_index = (i == 0)
+				? poly.size() - 1
+				: i - 1;
+
+	direction d = direction_in(poly[i], poly[prev_index], poly[next_index]);
+
+	return d == LEFT;
+}
+
+/** Checks whether the vertex `index` of the polygon `poly` is
+  * an ear
+  *
+  */
+template<typename T, std::size_t n_dimension>
+bool is_ear(const polygon<T, n_dimension>& poly, std::size_t index)
+{
+	if(poly.size() < 4)
+		return false;
+
+	if(is_reflex(poly, index))
+		return false;
+
+	size_t next_index = (index + 1) % poly.size();
+	size_t prev_index = (index == 0)
+				? poly.size() - 1
+				: index - 1;
+
+	for(	std::size_t i = (next_index + 1)%poly.size();
+		i != prev_index;
+		i = (i+1)%poly.size())
+	{
+		if(	direction_in(poly[prev_index], poly[index], poly[i])
+				== LEFT
+			&& direction_in(poly[index], poly[next_index], poly[i])
+				== LEFT
+			&& direction_in(poly[next_index], poly[prev_index], poly[i])
+				== LEFT){
+			return false;
+		}
+	}
+
+	return true;
+}
+
+/** Checks whether the vertex `index` of the polygon `poly` is
+  * a mouth
+  *
+  */
+template<typename T, std::size_t n_dimension>
+bool is_mouth(const polygon<T, n_dimension>& poly, std::size_t index)
+{
+	if(poly.size() < 4)
+		return false;
+
+	if(!is_reflex(poly, index))
+		return false;
+
+	size_t next_index = (index + 1) % poly.size();
+	size_t prev_index = (index == 0)
+				? poly.size() - 1
+				: index - 1;
+
+	for(	std::size_t i = (next_index + 1)%poly.size();
+		i != prev_index;
+		i = (i+1)%poly.size())
+	{
+		if(	direction_in(poly[prev_index], poly[index], poly[i])
+				== RIGHT
+			&& direction_in(poly[index], poly[next_index], poly[i])
+				== RIGHT
+			&& direction_in(poly[next_index], poly[prev_index], poly[i])
+				== RIGHT){
+			return false;
+		}
+	}
+
+	return true;
+}
+
+/**
+  * calculate if p0 is below p1
+  */
+template<typename T, std::size_t n_dimension>
+inline bool is_below(
+	const point<T, n_dimension>& p0,
+	const point<T, n_dimension>& p1)
+{
+	return p0.y() < p1.y() || (p0.y() == p1.y()
+			&& p0.x() > p1.x());
+}
+
+/**
+  * calculate if p0 is above p1
+  */
+template<typename T, std::size_t n_dimension>
+inline bool is_above(
+	const point<T, n_dimension>& p0,
+	const point<T, n_dimension>& p1)
+{
+	return !is_below(p0, p1);
+}
+
+/*
+ * classify the vertice i of the polygon
+ */
+template<typename T, std::size_t n_dimension>
+vertex_type classify(
+	const polygon<T, n_dimension>& poly,
+	size_t index)
+{
+	size_t next_index = (index+1) % poly.size();
+	size_t prev_index = (index == 0)
+				? poly.size() - 1
+				: index - 1;
+
+	bool prev_is_below = is_below(poly[prev_index], poly[index]);
+	bool next_is_below = is_below(poly[next_index], poly[index]);
+
+	if(prev_is_below != next_is_below)
+		return REGULAR;
+
+	direction d = direction_in(
+		poly[prev_index],
+		poly[index],
+		poly[next_index]
+	);
+
+	if(prev_is_below){
+		// start or split
+
+		switch(d){
+		case LEFT:
+			return START;
+		case RIGHT:
+			return SPLIT;
+		case ON:
+			return REGULAR;
+		}
+	}else{
+		// end or merge
+		switch(d){
+		case LEFT:
+			return END;
+		case RIGHT:
+			return MERGE;
+		case ON:
+			return REGULAR;
+		}
+	}
+
+	return REGULAR;
 }
 
 }
